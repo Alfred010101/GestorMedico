a31cdd72e1bbd2b625de476906e622298cf555a7
Alfredo Arista Desktop <arista21dominguez@gmail.com>
2023-10-30 21:54:35 -0600

    Inicializando ventana principal

---
+++ src/interfaz/VentanaPrincipal.java
@@ -0,0 +1,18 @@
+
+package interfaz;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author alfrdo
+ */
+
+public class VentanaPrincipal extends JFrame
+{
+
+    public VentanaPrincipal()
+    {
+    }
+    
+}
